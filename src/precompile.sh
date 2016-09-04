c2a381f7ef9ea0671c17c5c52bef7da53c934ea1
Hlopak <hlopak@users.noreply.github.com>
2016-09-04 18:32:41 +0300

    Fixed style '.text_styles ul > li::before'

--- src/precompile.sh
+++ src/precompile.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo -e "\n\
+  ver.1  \n\
+  Usage: \n\
+    precompile.sh sass  \n\
+    precompile.sh ts  \n\
+"
+
+
 [ "$1" == "sass" ]  &&  sass --watch ./src/sass:./css 
 
 [ "$1" == "ts" ]  &&  tsc --removeComments --outDir ./js/ -w ./src/ts/*.ts 
